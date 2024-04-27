1fe78880f18ae64e70fada601b587674163e9650
Roy-Praveen <roy.praveen18495@gmail.com>
2024-04-27 18:46:57 +0530

    Create bounding_box_regression.py

---
+++ bounding_box_regression.py
@@ -0,0 +1,9 @@
+import os, re, time, json, zipfile
+import PIL.Image, PIL.ImageFont, PIL.ImageDraw
+import numpy as np
+import tensorflow as tf
+from matplotlib import pyplot as plt
+import tensorflow_datasets as tfds
+import cv2
+####################################################
+
