07f31b71a1222b3536c38a9fcd8f7a087a85ec15
Roy-Praveen <roy.praveen18495@gmail.com>
2024-05-08 13:29:31 +0530

    fine_tuning_added

--- utils.py
+++ utils.py
@@ -221,6 +221,20 @@ def dataset_to_numpy_with_original_bboxes_util(dataset, batch_size=0, N=0):
 
     return np.array(ds_original_images, dtype='object'), np.array(ds_images, dtype='object'), np.array(ds_bboxes, dtype='object')
 
+
+def load_image_into_numpy_array(path):
+  """Load an image from file into a numpy array.
+
+  Puts image into numpy array to feed into tensorflow graph.
+  Note that by convention we put it into a numpy array with shape
+  (height, width, channels), where channels=3 for RGB.
+
+  """
+  img_data = tf.io.gfile.GFile(path, 'rb').read()
+  image = Image.open(BytesIO(img_data))
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
 ########################################################
 '''Utilities for visualization before training'''
 def get_visualization_training_dataset():
