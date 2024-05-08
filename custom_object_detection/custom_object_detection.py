5d2407c9fe07d41b68dec4452988f07815f946e0
Roy-Praveen <roy.praveen18495@gmail.com>
2024-05-08 15:17:19 +0530

    Create custom_object_detection.py

---
+++ custom_object_detection/custom_object_detection.py
@@ -0,0 +1,16 @@
+'''
+
+Here the main objective of this script is to fine tune an existing
+architecture from the Tensorflow Model Garden and finetune it to 
+perform a custom object detection.
+
+The process is simplified using TensorFlow object Detection API
+
+Dataset: Any dataset with annotations of the expected format
+Model: Default model used here is ResNet, but other models from model garden also be used
+OS: Implementation was originally done in google colab but scripts can be used for other operating systems also
+
+Recommended: Users are recommended to use a virtual environment to not affect other programs
+
+'''
+
