46435be23387edf7c535cec6644037d9dc3f2a69
Roy-Praveen <roy.praveen18495@gmail.com>
2024-05-08 12:19:27 +0530

    Adding fine tuning script using ResNet

---
+++ fine_tuning.py
@@ -0,0 +1,4 @@
+'''This is an object detection exercise using ResNet architecture.
+Here we fine-tune the model for 
+In this script ResNet model is loaded from the Model Graden of Tensorflow
+The classification head is customizable for usage
